f99c7b61d8930012446e04d1b5a2fa2e5cf8ba47
drgndenis <serkis-denis.dragan@ogr.iuc.edu.tr>
2023-10-28 18:11:20 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Denis DRAGAN on 28.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
